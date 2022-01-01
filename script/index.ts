d664f1f902537b93e2f16733010bab32cb45be95
Doug Bell <doug@preaction.me>
2021-12-31 19:58:03 -0600

    add esrun to run typescript scripts

---
+++ script/index.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env npx esrun
+
+/**
+ * This is a TypeScript file that can be executed directly. This uses
+ * `esrun` to do the real work.
+ */
+
+const args = process.argv.slice(2);
+console.log( args );
